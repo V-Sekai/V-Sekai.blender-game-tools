a5384384b786e6a94bff6f29e33988c15965acb7
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2022-11-27 09:20:28 -0800

    Update mesh_blender_only_tapered.py

--- misc/mesh_blender_only_tapered.py
+++ misc/mesh_blender_only_tapered.py
@@ -6,24 +6,93 @@ from mathutils import Vector
 from math import *
 
 def CreateMesh():
-
     obj = bpy.context.active_object
 
-    if obj == None:
-        self.report({"ERROR"}, "No selection")
-    elif obj.type != "ARMATURE":
-        self.report({"ERROR"}, "Armature expected")
-    else:
-        processArmature(bpy.context, obj)
+    if obj != None and obj.type == "ARMATURE":
+        context = bpy.context
+        arm = obj
+        genVertexGroups = True
+        # Process the armature, goes through its bones and creates the mesh
+        print("processing armature {0}".format(arm.name))
 
+        # Creates the mesh object        
+        # Create the base object from the armature
+        name = arm.name + "_mesh"
+        meshData = bpy.data.meshes.new(name + "Data")
+        meshObj = bpy.data.objects.new(name, meshData)
+        meshObj.matrix_world = arm.matrix_world.copy()
+        
+        context.collection.objects.link(meshObj)
 
-# Create the base object from the armature
-def meshFromArmature(arm):
-    name = arm.name + "_mesh"
-    meshData = bpy.data.meshes.new(name + "Data")
-    meshObj = bpy.data.objects.new(name, meshData)
-    meshObj.matrix_world = arm.matrix_world.copy()
-    return meshObj
+        verts = []
+        edges = []
+        faces = []
+        vertexGroups = {}
+
+        bpy.ops.object.mode_set(mode="EDIT")
+
+        try:
+            # Goes through each bone
+            for editBone in [b for b in arm.data.edit_bones if b.use_deform]:
+                boneName = editBone.name
+                # print( boneName )
+                poseBone = arm.pose.bones[boneName]
+
+                # Gets edit bone informations
+                editBoneHead = editBone.head
+                editBoneTail = editBone.tail
+                editBoneVector = editBoneTail - editBoneHead
+                editBoneSize = editBoneVector.dot(editBoneVector)
+                editBoneRoll = editBone.roll
+                editBoneX = editBone.x_axis
+                editBoneZ = editBone.z_axis
+                editBoneHeadRadius = editBone.head_radius
+                editBoneTailRadius = editBone.tail_radius
+
+                # Creates the mesh data for the bone
+                baseIndex = len(verts)
+                baseSize = sqrt(editBoneSize)
+                newVerts, newFaces = boneGeometry(
+                    editBoneHead,
+                    editBoneTail,
+                    editBoneX,
+                    editBoneZ,
+                    baseSize,
+                    editBoneHeadRadius,
+                    editBoneTailRadius,
+                    baseIndex
+                )
+
+                verts.extend(newVerts)
+                faces.extend(newFaces)
+
+                # Creates the weights for the vertex groups
+                vertexGroups[boneName] = [(x, 1.0) for x in range(baseIndex, len(verts))]
+
+            # Assigns the geometry to the mesh
+            meshObj.data.from_pydata(verts, edges, faces)
+
+        except:
+            bpy.ops.object.mode_set(mode="OBJECT")
+        else:
+            bpy.ops.object.mode_set(mode="OBJECT")
+
+        # Assigns the vertex groups
+        if genVertexGroups:
+            for name, vertexGroup in vertexGroups.items():
+                groupObject = meshObj.vertex_groups.new(name=name)
+                for (index, weight) in vertexGroup:
+                    groupObject.add([index], weight, "REPLACE")
+
+        # Creates the armature modifier
+        modifier = meshObj.modifiers.new("ArmatureMod", "ARMATURE")
+        modifier.object = arm
+        modifier.use_bone_envelopes = False
+        modifier.use_vertex_groups = True
+
+        meshObj.data.update()
+
+        return meshObj
 
 
 # Create the bone geometry (vertices and faces)
@@ -56,86 +125,6 @@ def boneGeometry(l1, l2, x, z, baseSize, l1Size, l2Size, base):
     ]
 
     return verts, faces
-
-
-# Process the armature, goes through its bones and creates the mesh
-def processArmature(context, arm, genVertexGroups=True):
-    print("processing armature {0}".format(arm.name))
-
-    # Creates the mesh object
-    meshObj = meshFromArmature(arm)
-    context.collection.objects.link(meshObj)
-
-    verts = []
-    edges = []
-    faces = []
-    vertexGroups = {}
-
-    bpy.ops.object.mode_set(mode="EDIT")
-
-    try:
-        # Goes through each bone
-        for editBone in [b for b in arm.data.edit_bones if b.use_deform]:
-            boneName = editBone.name
-            # print( boneName )
-            poseBone = arm.pose.bones[boneName]
-
-            # Gets edit bone informations
-            editBoneHead = editBone.head
-            editBoneTail = editBone.tail
-            editBoneVector = editBoneTail - editBoneHead
-            editBoneSize = editBoneVector.dot(editBoneVector)
-            editBoneRoll = editBone.roll
-            editBoneX = editBone.x_axis
-            editBoneZ = editBone.z_axis
-            editBoneHeadRadius = editBone.head_radius
-            editBoneTailRadius = editBone.tail_radius
-
-            # Creates the mesh data for the bone
-            baseIndex = len(verts)
-            baseSize = sqrt(editBoneSize)
-            newVerts, newFaces = boneGeometry(
-                editBoneHead,
-                editBoneTail,
-                editBoneX,
-                editBoneZ,
-                baseSize,
-                editBoneHeadRadius,
-                editBoneTailRadius,
-                baseIndex
-            )
-
-            verts.extend(newVerts)
-            faces.extend(newFaces)
-
-            # Creates the weights for the vertex groups
-            vertexGroups[boneName] = [(x, 1.0) for x in range(baseIndex, len(verts))]
-
-        # Assigns the geometry to the mesh
-        meshObj.data.from_pydata(verts, edges, faces)
-
-    except:
-        bpy.ops.object.mode_set(mode="OBJECT")
-    else:
-        bpy.ops.object.mode_set(mode="OBJECT")
-
-    # Assigns the vertex groups
-    if genVertexGroups:
-        for name, vertexGroup in vertexGroups.items():
-            groupObject = meshObj.vertex_groups.new(name=name)
-            for (index, weight) in vertexGroup:
-                groupObject.add([index], weight, "REPLACE")
-
-    # Creates the armature modifier
-    modifier = meshObj.modifiers.new("ArmatureMod", "ARMATURE")
-    modifier.object = arm
-    modifier.use_bone_envelopes = False
-    modifier.use_vertex_groups = True
-
-    meshObj.data.update()
-
-    return meshObj
-
 
 class MeshFromArmatureOperator(bpy.types.Operator):
     bl_idname = "wm.mesh_from_armature"
