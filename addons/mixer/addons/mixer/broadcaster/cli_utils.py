b40cc0ec230c78bd5a6016a08bd02b193cac7b78
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-12 16:32:21 -0700

    Refactor CLI argument parsing and logging configuration
    - Removed unnecessary logging imports and functions
    - Renamed `init_logging` function to `add_cli_args`
    - Updated CLI argument names for log level and log file

--- addons/mixer/addons/mixer/broadcaster/cli_utils.py
+++ addons/mixer/addons/mixer/broadcaster/cli_utils.py
@@ -21,44 +21,15 @@
 # SOFTWARE.
 
 """
-Utility functions for CLI applications, mostly related to logging and argument parsing.
+Utility functions for CLI applications, mostly related to argument parsing.
 """
 
-import logging
-import logging.handlers
 import argparse
 
 
-def init_logging(args):
+def add_cli_args(parser: argparse.ArgumentParser):
     """
-    Initialize root logger of an application.
+    Set CLI arguments for configuration.
     """
-    log_numeric_level = getattr(logging, args.log_level.upper(), None)
-    if not isinstance(log_numeric_level, int):
-        raise ValueError(f"Invalid log level: {args.log_level}")
-
-    formatter = logging.Formatter(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(pathname)s:%(lineno)d]")
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-
-    logger = logging.getLogger()
-
-    logger.addHandler(handler)
-    logger.setLevel(log_numeric_level)
-
-    if args.log_file:
-        max_bytes = 1024 * 1000 * 512  # 512 MB
-        backup_count = 1024 * 1000  # 1B backup files
-        handler = logging.handlers.RotatingFileHandler(args.log_file, maxBytes=max_bytes, backupCount=backup_count)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
-        logger.info(f"Logging to file {args.log_file}")
-
-
-def add_logging_cli_args(parser: argparse.ArgumentParser):
-    """
-    Set CLI arguments for logger configuration.
-    """
-    parser.add_argument("--log-level", default="WARNING", help="Level of log to use by default.")
-    parser.add_argument("--log-file", help="Path to log file.")
+    parser.add_argument("--level", default="WARNING", help="Level to use by default.")
+    parser.add_argument("--file", help="Path to file.")
