5f8f6023097ada59b2f642f635cc84415cd1e011
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-04-08 05:50:25 -0700

    Remove VRM needs to be patched.

--- addons/VRM-Addon-for-Blender/src/io_scene_vrm/README.txt
+++
@@ -1,3 +0,0 @@
-# VRM Add-on for Blender
-
-See https://vrm-addon-for-blender.info for more information.
--- addons/VRM-Addon-for-Blender/tools/format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-cd "$(dirname "$0")/.."
-
-git ls-files "*.py" | xargs poetry run ruff format
-git ls-files "*.py" | xargs poetry run ruff check --fix
-git ls-files "*.sh" | xargs shfmt -w -s
